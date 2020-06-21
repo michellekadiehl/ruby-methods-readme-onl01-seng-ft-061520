4777643e32c4e892cf2e4f6743c9a46571ecb788
michelle diehl <michellekadiehl@gmail.com>
2020-06-21 05:19:32 +0000

    Done.

---
+++ touch greeting.rb
@@ -0,0 +1,17 @@
+def greeting
+  puts "Hello World"
+end
+def greeting
+  puts "Hello World"
+end
+
+greeting
+def greeting
+  puts "Hello World"
+end
+
+greeting
+greeting
+greeting
+greeting
+greeting
